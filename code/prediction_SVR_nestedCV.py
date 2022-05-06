acccf2f0ad4009a8efc294bd4236d3ce6d4a0b5c
Martin Gell <martygell@gmail.com>
2022-05-06 16:03:42 +0200

    adding learning curve option

--- code/prediction_SVR_nestedCV.py
+++ code/prediction_SVR_nestedCV.py
@@ -14,9 +14,7 @@ from sklearn.model_selection import ShuffleSplit, cross_validate, learning_curve
 
 
 ### Set params ###
-#pipe = 'enet'
 pipe = sys.argv[4]
-
 #FC_file = 'seitzman_nodes_average_runs_REST1_REST1_REST2_REST2-subs_651-params_FC_gm_FSL025_no_overlap_dt_flt0.1_0.01.csv'
 FC_file = sys.argv[1]
 #beh_file = 'beh_HCP_A_motor.csv'
@@ -27,11 +25,18 @@ beh = sys.argv[3]
 k_inner = 5             # k folds for hyperparam search
 k_outer = 10            # k folds for CV
 n_outer = 5             # n repeats for CV
-rs = 123456             # random state: int or None
+rs = 123456             # random state: int for reproducibility or None
 
 designator = 'test'     # char designation of output file
 val_split = False       # Split data to train and held out validation?
 val_split_size = 0.2    # Size of validation held out sample
+
+res_folder = 'subsamples'
+
+# array or empty array (np.empty(0)) of subsamples to simulate
+subsample = np.empty(0) #np.array([195,295,395]) these are only train + 55 test makes 250, 350 and 450
+#subsample = np.array([195,295,395])
+#k_sample = 100
 
 score_pearson = metrics.make_scorer(cor_true_pred_pearson, greater_is_better=True)
 score_spearman = metrics.make_scorer(cor_true_pred_spearman, greater_is_better=True)
@@ -49,7 +54,9 @@ print(f'Running prediction with {model}')
 #wd = Path('/data/project/impulsivity/Prediction_HCP')
 wd = os.getcwd()
 wd = Path(os.path.dirname(wd))
-out_dir = wd / 'res' # / 'test_mean100'
+out_dir = wd / 'res' 
+if res_folder:
+    out_dir = out_dir / res_folder
 
 # load behavioural measures
 path2beh = wd / 'text_files' / beh_file
@@ -92,6 +99,8 @@ outer_cv = RepeatedKFold(n_splits=k_outer, n_repeats=n_outer, random_state=rs)
 # Run CV 
 if nested == 1: # Nested CV with parameter optimization
     print('Using nested CV..')
+    print('Hyperparam search over:')
+    print(f'{grid}')
     grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=1,
         cv=inner_cv, scoring="neg_root_mean_squared_error", verbose=3) # on Juseless n_jobs=None    
     scores = cross_validate(grid_search, X, np.ravel(y), scoring=scoring, cv=outer_cv,
@@ -106,6 +115,8 @@ elif nested == 0: # non-nested CV
         return_train_score=True, return_estimator=True, verbose=3, n_jobs=1)
     # results
     cv_res = pd.DataFrame(scores)
+    if pipe == 'ridgeCV':
+        for i in scores['estimator']: print(i.alpha_)
 
 mean_accuracy = cv_res.mean()
 print(f'Overall accuracy:')
@@ -165,3 +176,42 @@ if val_split:
 
     out_file = out_dir / 'predicted' / f"{designator}predicted_res.csv"
     np.savetxt(out_file, y_pred, delimiter=',')
+
+if subsample.any():
+    print('Computing learning curve..')
+    # cv
+    outer_cv = ShuffleSplit(n_splits=k_sample, test_size=0.1, random_state=rs)
+    #outer_cv = RepeatedKFold(n_splits=k_outer, n_repeats=n_outer, random_state=rs)
+    grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=1,
+        cv=inner_cv, scoring="neg_root_mean_squared_error", verbose=3) # on Juseless n_jobs=None
+    #search_res = grid_search.fit(X, np.ravel(y))
+    scores = learning_curve(grid_search, X, np.ravel(y), train_sizes=subsample, return_times=True, shuffle=True,
+        cv=outer_cv, scoring='r2', verbose=3, n_jobs=1, random_state=rs)
+    # results
+    train_size, train_scores, test_scores = scores[:3]
+    #train_errors, test_errors = np.transpose(-train_scores), np.transpose(-test_scores)
+    train_errors = np.transpose(train_scores)
+    test_errors = np.transpose(test_scores)
+    cols = list()
+    #for i in train_size[:]: cols.append(f'x{str(i)}')
+    for i in train_size[:]: cols.append(i+55)
+
+    sample_test_res = pd.DataFrame(test_errors, columns=cols)
+    sample_train_res = pd.DataFrame(train_errors, columns=cols)
+    
+    # save train
+    out_file = out_dir / 'learning_curve'
+    out_file.mkdir(parents=True, exist_ok=True)
+    out_file = out_file / f"train_{pipe}-source_{''.join(FC_file.split('.')[0:-1])}-beh_{beh_f[len(beh_f)-1]}_{beh}-rseed_{rs}-cv_res.csv"
+    print(f'saving: {out_file}')
+    sample_train_res.to_csv(out_file, index=False)
+
+    # save test
+    out_file = out_dir / 'learning_curve'
+    out_file = out_file / f"test_{pipe}-source_{''.join(FC_file.split('.')[0:-1])}-beh_{beh_f[len(beh_f)-1]}_{beh}-rseed_{rs}-cv_res.csv"
+    print(f'saving: {out_file}')
+    sample_test_res.to_csv(out_file, index=False)
+
+    #cv_res = pd.DataFrame(scores)
+    #print(f'Best hyperparams from nested CV {n_outer}x{k_outer}x{k_inner}:')
+    #for i in cv_res.loc[:,'estimator']: print(i.best_estimator_)
