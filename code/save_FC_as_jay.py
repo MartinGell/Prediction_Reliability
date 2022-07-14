6df19896413f8e0948a4184c26325ebaa3e354ba
Martin Gell <martygell@gmail.com>
2022-07-14 16:52:32 +0200

    updated script for Amir

--- code/save_FC_as_jay.py
+++ code/save_FC_as_jay.py
@@ -1,10 +1,20 @@
 
 # Load all files, unpickle them, and save them to a new file
 
-import os
 import numpy as np
 import pandas as pd
 import datatable as dt
+
+# paths
+path2beh = '/tmp/tmp_amir/for_Martin'
+outpath = '/data/project/ukb_reliability_in_prediction/input'
+
+# Load beh_file
+#beh = pd.read_csv(f'{path2beh}/UKB_5000_subs_FC_behav.csv')
+x = np.linspace(0,99,100)
+x = np.delete(x,41)
+beh = pd.DataFrame(x, columns=['eid'])
+sampled_subs = beh['eid']
 
 # save into one table
 nodes = 400
@@ -12,11 +22,8 @@ cols = ((nodes*nodes)-nodes)/2
 data = np.empty((0,int(cols))) # data should be subs*regions
 subs = np.empty((0,1))         # subs should be subs*1
 
-# list files
-in_path = os.getcwd()
-files = os.listdir(in_path)
-
-for f_i in files:
+for sub in sampled_subs:
+    f_i = f'FC_{int(sub)}.npz'
     f = np.load(f_i)
     fc = f['correlation_matrix']
 
@@ -30,20 +37,24 @@ for f_i in files:
         #print('NOT REMOVNG')
 
     # zscore
-    zFC = np.arctanh(FC)
+    #zFC = np.arctanh(FC) #dont do this, its done later
 
     # save
-    data = np.append(data, np.atleast_2d(zFC), axis=0)
+    data = np.append(data, np.atleast_2d(FC), axis=0)
 
     # save sub id
     name = f_i.split('_')
     subs = np.append(subs, np.atleast_2d(name[-1].split('.')[0]), axis=0)
 
 # Save to a new file
-print('Saving...')   
 d = pd.concat([pd.DataFrame(subs, columns=['subID']), pd.DataFrame(data)], axis=1)
+
+# Save as .jay
+print('Saving...')
+out = f'{outpath}/Schaefer400x17_nodenoise_UKB_5000.jay'
+
 DT = dt.Frame(d)
-DT.to_jay("/tmp/tmp_amir/for_Martin/Schaefer400x17_nodenoise_UKB_5000.jay")
+DT.to_jay(out)
 
 #DT.to_jay("/data/project/impulsivity/prediction_simulations/input/test.jay")
 
